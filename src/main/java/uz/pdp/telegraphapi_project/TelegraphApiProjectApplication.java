7b527cc2a6a62de92c859405b031e287e826b2c3
nurabdu8000 <nurabdu8000@gmail.com>
2023-05-29 15:35:19 +0500

    Initial commit

---
+++ src/main/java/uz/pdp/telegraphapi_project/TelegraphApiProjectApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.telegraphapi_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegraphApiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegraphApiProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/telegraphapi_project/TelegraphApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.telegraphapi_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegraphApiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
